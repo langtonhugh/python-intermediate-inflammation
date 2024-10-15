6f65831c9e969bd69aa816029d08d11dc796276b
Sam Langton <36928709+langtonhugh@users.noreply.github.com>
2024-10-15 11:56:43 +0200

    add std test
    Co-authored-by: Brecht Nijman <79448658+brechtfm@users.noreply.github.com>

--- tests/test_models.py
+++ tests/test_models.py
@@ -37,7 +37,7 @@ def test_daily_mean_integers():
     ([1.0, 1.0, 1.0], 0),
     ([0.0, 2.0], 1.0)
 ])
-def test_daily_standard_deviation(data, expected_standard_deviation):
+def test_daily_standard_deviation(test, expected):
     from inflammation.models import s_dev
     result_data = s_dev(data)['standard deviation']
     npt.assert_approx_equal(result_data, expected_standard_deviation)
